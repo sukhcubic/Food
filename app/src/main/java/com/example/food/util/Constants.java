97e23489967f97269ce1bef0f0b0b16223c9f61b
sukhjinderji.singh@gmail.com <sukhjinder.singh@nextbus.com>
2019-07-18 23:43:45 -0500

    Adding Key + constants

---
+++ app/src/main/java/com/example/food/util/Constants.java
@@ -0,0 +1,24 @@
+package com.example.food.util;
+
+
+public class Constants {
+
+    public static final String BASE_URL = "https://www.food2fork.com";
+    public static final int NETWORK_TIMEOUT = 3000;
+    public static final String API_KEY = "135f4f86097ec26b729151dad4e94f82";
+
+    public static final String[] DEFAULT_SEARCH_CATEGORIES =
+            {"Barbeque", "Breakfast", "Chicken", "Beef", "Brunch", "Dinner", "Wine", "Italian"};
+
+    public static final String[] DEFAULT_SEARCH_CATEGORY_IMAGES =
+            {
+                    "barbeque",
+                    "breakfast",
+                    "chicken",
+                    "beef",
+                    "brunch",
+                    "dinner",
+                    "wine",
+                    "italian"
+            };
+}
